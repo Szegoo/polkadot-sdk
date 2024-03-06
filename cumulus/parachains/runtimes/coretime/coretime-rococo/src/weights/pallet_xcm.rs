150d4921cdad4540b2af7142ee70c16e48405549
Szegoo <sakacszergej@gmail.com>
2024-03-06 09:23:05 +0100

    patch

--- cumulus/parachains/runtimes/coretime/coretime-rococo/src/weights/pallet_xcm.rs
+++ cumulus/parachains/runtimes/coretime/coretime-rococo/src/weights/pallet_xcm.rs
@@ -99,7 +99,7 @@ impl<T: frame_system::Config> pallet_xcm::WeightInfo for WeightInfo<T> {
 		//  Measured:  `0`
 		//  Estimated: `0`
 		// Minimum execution time: 18_446_744_073_709_551_000 picoseconds.
-		Weight::from_parts(18_446_744_073_709_551_000, 0)
+		Weight::from_parts(0, 0)
 			.saturating_add(Weight::from_parts(0, 0))
 	}
 	/// Storage: `Benchmark::Override` (r:0 w:0)
